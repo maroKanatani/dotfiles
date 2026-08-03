bcc7b66f99366da050ef16e73ca575e7c3bad4ae
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2026-08-04 00:09:49 +0900

    chore: add some tools

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,4 +1,12 @@
 { pkgs }:
 {
+  bat = pkgs.bat;
+  eza = pkgs.eza;
+  fd = pkgs.fd;
+  fzf = pkgs.fzf;
+  gh = pkgs.gh;
   jq = pkgs.jq;
+  ripgrep = pkgs.ripgrep;
+  tree = pkgs.tree;
+  yq = pkgs.yq;
 }
